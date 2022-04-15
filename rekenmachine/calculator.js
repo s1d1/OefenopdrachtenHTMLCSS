cec854770847790f674af773403dc00ae29bb540
s1d1 <sarah.danner@upcmail.nl>
2022-04-15 17:18:25 +0200

    eerste stap rekenmachine knoppen

--- rekenmachine/calculator.js
+++ rekenmachine/calculator.js
@@ -1,9 +1,35 @@
-// functie dat de waardes van de knoppen op het scherm laten zien
+formula = [];
 
-
-
-// functie dat de som op het scherm uitrekent en antwoord laat zien
-
-
-
-// functie voor speciale knoppen (C/CE, OFF, )
+function clickNumber(button) {
+    const number = button.value;
+    if (number == "0"){
+        document.getElementById("screen").value = "0";
+    }
+    else if (number == "1"){
+        document.getElementById("screen").value = "1";
+    }
+    else if (number == "2"){
+        document.getElementById("screen").value = "2";
+    }
+    else if (number == "3"){
+        document.getElementById("screen").value = "3";
+    }
+    else if (number == "4"){
+        document.getElementById("screen").value = "4";
+    }
+    else if (number == "5"){
+        document.getElementById("screen").value = "5";
+    }
+    else if (number == "6"){
+        document.getElementById("screen").value = "6";
+    }
+    else if (number == "7") {
+      document.getElementById("screen").value = "7";
+    }
+    else if (number == "8"){
+        document.getElementById("screen").value = "8";
+    }
+    else if (number == "9"){
+        document.getElementById("screen").value = "9";
+    }
+  }
