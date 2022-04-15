77b2db38526128050ee6f6263af30370af66cba5
s1d1 <sarah.danner@upcmail.nl>
2022-04-15 15:40:47 +0200

    start van rekenmachine

---
+++ rekenmachine/calculator.js
@@ -0,0 +1,9 @@
+// functie dat de waardes van de knoppen op het scherm laten zien
+
+
+
+// functie dat de som op het scherm uitrekent en antwoord laat zien
+
+
+
+// functie voor speciale knoppen (C/CE, OFF, )
